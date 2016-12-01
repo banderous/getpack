9c324c9d6829f51e36cf4ae277b26ed6776bd065
Alex McAusland <alex.mcausland@gmail.com>
2016-12-01 15:17:59 +0000

    Adding searching for unity versions.

--- client/plugin/src/main/groovy/com/nxt/UnityLauncher.groovy
+++ client/plugin/src/main/groovy/com/nxt/UnityLauncher.groovy
@@ -16,4 +16,34 @@ class UnityLauncher {
         def  version = versionFile.readLines().get(0)
         return version.split(":")[1].trim()
     }
+
+    public static File UnityPathForVersion(File searchPath, String version) {
+        def result = searchPath.listFiles().find { file ->
+            if (file.isDirectory()) {
+                def pList = new File(file, "Unity.app/Contents/Info.plist")
+                if (pList.exists()) {
+                    def parser = new XmlSlurper()
+                    parser.setFeature("http://apache.org/xml/features/disallow-doctype-decl", false)
+                    def next = false
+                    // Search for bundle version.
+                    def node = parser.parse(pList).dict.'*'.find { node ->
+                        if (next) {
+                            return true
+                        }
+                        next = node.name() == "key" && node.text() == "CFBundleVersion"
+                        false
+                    }
+
+                    if (node.text() == version) {
+                        return file
+                    }
+                }
+            }
+        }
+
+        if (!result) {
+            throw new IllegalArgumentException("Unity ${version} not found")
+        }
+        result
+    }
 }
--- client/plugin/src/test/groovy/UnityLauncherSpec.groovy
+++ client/plugin/src/test/groovy/UnityLauncherSpec.groovy
@@ -26,4 +26,28 @@ class UnityLauncherSpec extends Specification {
         then:
         thrown(IllegalArgumentException)
     }
+
+    def "finds the install directory of the specified unity version"() {
+        when:
+        def searchPath = new File("src/test/resources/Applications")
+        def unityPath = UnityLauncher.UnityPathForVersion(searchPath, version)
+
+        then:
+        unityPath instanceof File
+        unityPath == new File("src/test/resources", expectedPath);
+
+        where:
+        version| expectedPath
+        "5.0.0p3"| "Applications/Unity 5.0.0p3"
+        "5.3.5f1"| "Applications/Unity 5.3.5f1"
+    }
+
+    def "throws if unity with version not found"() {
+        when:
+        def searchPath = new File("src/test/resources/Applications")
+        UnityLauncher.UnityPathForVersion(searchPath, "nonsense")
+
+        then:
+        thrown(IllegalArgumentException)
+    }
 }
